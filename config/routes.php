410074594f9274aed5e03088759efa3c74280634
Tesemir <madi41674@gmail.com>
2025-06-28 23:03:09 +0500

    Added: Controllers, Routes, Kernel and entry point

---
+++ config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Controllers\HomeController;
+use App\Controllers\MovieController;
+use App\Kernel\Router\Route;
+
+return [
+    Route::get('/home', [HomeController::class, 'index']),
+    Route::get('/movies', [MovieController::class, 'index']),
+    Route::get('/admin/movies/add', [MovieController::class, 'add']),
+    Route::post('/admin/movies/add', [MovieController::class, 'store']),
+    Route::get('/test', function() {
+        echo 'test';
+    })
+];
---
+++ src/Controllers/MovieController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Kernel\Controller\Controller;
+use App\Kernel\View\View;
+
+class MovieController extends Controller
+{
+    public function index(): void
+    {
+        $this->view('movies');
+    }
+
+    public function add(): void
+    {
+        $this->view('admin/movies/add');
+    }
+
+    public function store(): void
+    {
+        dd($this->request()->input('name'));
+    }
+}
