777b704bf648a9906e903f72a73993978c2982c2
Tesemir <madi41674@gmail.com>
2025-06-29 19:23:26 +0500

    Added: Storage, Middlewares for guest and auth

--- kernel/Controller/Controller.php
+++ kernel/Controller/Controller.php
@@ -2,37 +2,52 @@
 
 namespace App\Kernel\Controller;
 
-use App\Kernel\Http\Redirect;
-use App\Kernel\Http\Request;
-use App\Kernel\Session\Session;
+use App\Kernel\Auth\AuthInterface;
+use App\Kernel\Database\DatabaseInterface;
+use App\Kernel\Http\RedirectInterface;
+use App\Kernel\Http\RequestInterface;
+use App\Kernel\Session\SessionInterface;
+use App\Kernel\Storage\StorageInterface;
 use App\Kernel\View\View;
+use App\Kernel\View\ViewInterface;
 
 abstract class Controller
 {
-    private View $view;
-    private Request $request;
-    private Redirect $redirect;
-    private Session $session;
-    public function view(string $name): void
+    private ViewInterface $view;
+
+    private RequestInterface $request;
+
+    private RedirectInterface $redirect;
+
+    private SessionInterface $session;
+
+    private DatabaseInterface $database;
+
+    private AuthInterface $auth;
+
+    private StorageInterface $storage;
+
+    public function view(string $name, array $data = [], string $title = ''): void
     {
-        $this->view->page($name);
+        $this->view->page($name, $data, $title);
     }
+
     public function setView(View $view): void
     {
         $this->view = $view;
     }
 
-    public function request(): Request
+    public function request(): RequestInterface
     {
         return $this->request;
     }
 
-    public function setRequest(Request $request): void
+    public function setRequest(RequestInterface $request): void
     {
         $this->request = $request;
     }
 
-    public function setRedirect(Redirect $redirect): void
+    public function setRedirect(RedirectInterface $redirect): void
     {
         $this->redirect = $redirect;
     }
@@ -42,13 +57,43 @@ abstract class Controller
         $this->redirect->to($url);
     }
 
-    public function session(): Session
+    public function session(): SessionInterface
     {
         return $this->session;
     }
 
-    public function setSession(Session $session): void
+    public function setSession(SessionInterface $session): void
     {
         $this->session = $session;
+    }
+
+    public function db(): DatabaseInterface
+    {
+        return $this->database;
+    }
+
+    public function setDatabase(DatabaseInterface $database): void
+    {
+        $this->database = $database;
+    }
+
+    public function auth(): AuthInterface
+    {
+        return $this->auth;
+    }
+
+    public function setAuth(AuthInterface $auth): void
+    {
+        $this->auth = $auth;
+    }
+
+    public function storage(): StorageInterface
+    {
+        return $this->storage;
+    }
+
+    public function setStorage(StorageInterface $storage): void
+    {
+        $this->storage = $storage;
     }
 }
--- kernel/View/View.php
+++ kernel/View/View.php
@@ -2,50 +2,64 @@
 
 namespace App\Kernel\View;
 
+use App\Kernel\Auth\AuthInterface;
 use App\Kernel\Exceptions\ViewNotFoundException;
-use App\Kernel\Session\Session;
+use App\Kernel\Session\SessionInterface;
+use App\Kernel\Storage\StorageInterface;
 
-class View
+class View implements ViewInterface
 {
+    private string $title;
 
     public function __construct(
-        private Session $session,
-    )
-    {
-
+        private SessionInterface $session,
+        private AuthInterface $auth,
+        private StorageInterface $storage
+    ) {
     }
 
-    public function page(string $name): void
+    public function page(string $name, array $data = [], string $title = ''): void
     {
+        $this->title = $title;
 
-        $viewPath = APP_PATH . "/views/pages/$name.php";
+        $viewPath = APP_PATH."/views/pages/$name.php";
 
-        if (!file_exists($viewPath)) {
+        if (! file_exists($viewPath)) {
             throw new ViewNotFoundException("View $name not found");
         }
 
-        extract($this->defaultData());
+        extract(array_merge($this->defaultData(), $data));
 
         include_once $viewPath;
     }
 
-    public function component(string $name): void
+    public function component(string $name, array $data = []): void
     {
-        $componentPath = APP_PATH . "/views/components/$name.php";
+        $componentPath = APP_PATH."/views/components/$name.php";
 
-        if (!file_exists($componentPath)) {
+        if (! file_exists($componentPath)) {
             echo "Component $name not found";
+
             return;
         }
 
-        include_once $componentPath;
+        extract(array_merge($this->defaultData(), $data));
+
+        include $componentPath;
     }
 
     private function defaultData(): array
     {
-        return[
+        return [
             'view' => $this,
             'session' => $this->session,
+            'auth' => $this->auth,
+            'storage' => $this->storage,
         ];
+    }
+
+    public function title(): string
+    {
+        return $this->title;
     }
 }
